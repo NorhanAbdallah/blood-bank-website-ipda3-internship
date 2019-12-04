1dc628e1da4652dec582f7e21416885955a77668
Norhan Abdallah <norhanabdallah96@gmail.com>
2019-12-04 17:59:44 +0200

    first commit

---
+++ Assets/js/JavaScript.js
@@ -0,0 +1,90 @@
+
+  $(document).ready(function(){
+  
+  $('.cover-one-slider').slick({
+    rtl: true,
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        } 
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+  $('.articles-cards').slick({
+    rtl: true,
+    dots: false,
+    infinite: false,
+    nextArrow: '.arrow-right-offer',
+    prevArrow:'.arrow-left-offer',
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        } 
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        } 
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  
+
+  $(function () {
+    $('#datetimepicker1').datetimepicker();
+});
+
+});
